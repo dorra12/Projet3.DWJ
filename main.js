8a8bcf7821357e5653d729bd7022602a28481f1c
dorra12 <48134582+dorra12@users.noreply.github.com>
2019-08-25 22:23:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -52,12 +52,13 @@ request.send();
   /*onload :*/
 request.onload = function() {
 	var stationsJCD = request.response;
+	console.log(stationsJCD);
 	stationsJCD.forEach(function (stationJCD){	
 		myMap.addMarqueur(stationJCD);
 	})
 }
  
-
+console.log(myMap);
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
