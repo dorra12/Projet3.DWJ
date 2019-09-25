a5a1636728dd12a1ea8c702faecb34e96556a4a0
dorra12 <48134582+dorra12@users.noreply.github.com>
2019-09-25 18:31:37 +0200

    Add files via upload

---
+++ marqueur.js
@@ -0,0 +1,37 @@
+class Marqueur {
+    constructor(stationJCD) {
+        this.coordinates = [stationJCD.position.lng, stationJCD.position.lat];
+        this.station = new StationVelo(stationJCD);
+        this.nonReservable = document.getElementById("station_non_reservable");
+        this.buttonform = document.getElementById("buttonform");
+        this.marqueurCible = document.getElementsByClassName("marker");
+    }
+    colorStatus() {
+
+        if (this.station.status === "OPEN") {
+            document.getElementsByClassName("statusValue")[0].style.color = "green";
+        }
+    }
+    nonResa() {
+
+        if (this.station.availableBikes == 0) {
+            this.nonReservable.style.display = "block";
+            this.buttonform.style.display = "none"
+        } else {
+            this.nonReservable.style.display = "none";
+            this.buttonform.style.display = "block"
+        }
+    }
+    getMarkerImage() {
+        var imgMarker;
+
+        if (this.station.availableBikes == 0) {
+            imgMarker = "url('Images/MarkerRouge.png')";
+        } else if (this.station.availableBikes < 4) {
+            imgMarker = "url('Images/MarkerOrange.png')";
+        } else {
+            imgMarker = "url('Images/MarkerVert.png')";
+        }
+        return imgMarker;
+    }
+}
